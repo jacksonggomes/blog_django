b6fbfdfe9810f94e70568889c806d330907b9fd6
Jackson Gomes <jgg.snf@uea.edu.br>
2022-08-17 14:50:21 +0000

    final

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("hello")
+posts = [
+{
+    "title": " Meu primeiro post",
+    "body": "Esse post é o primeiro do blog",
+    "author": "Danilo de Souza Miguel",
+    "created": datetime(2022,8,17)
+},
+{
+    "title": " Meu segundo post",
+    "body": "Esse post é o segundo do blog",
+    "author": "Raiane Caroline",
+    "created": datetime(2022,8,17)
+},
+{
+    "title": " Meu terceiro post",
+    "body": "Esse post é o terceito do blog",
+    "author": "Jezimiel Marcondes",
+    "created": datetime(2022,8,17)
+},
+]
 
 @app.route("/")
-def hello():
-    return "Hello World"
-
-@app.route("/usuarios")
-def usuario():
-    usuario = [1, "Jackson Gomes", "Professor"]
-    aluno = ["André Guedes", "Lucas Santos", "Alícia Duarte", "Raiane Caroline"]
-    return render_template("index.html", usuario=usuario, aluno=aluno)
-
-@app.route("/contatos")
-def contato():
-    nomeAula = "Aula Python para Web"
-    return render_template("index.html", nome=nomeAula)
+def index():
+    return render_template("index.html", posts=posts)
